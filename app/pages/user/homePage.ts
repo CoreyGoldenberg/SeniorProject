7ef9e736eaf79a90b75655ba48f1b9947d0ec733
Adtiv <datocco13@gmail.com>
2016-09-20 14:51:15 -0400

    firebase

--- app/pages/user/homePage.ts
+++ app/pages/user/homePage.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { LoginPage} from './login.component';
 import { SignUpPage } from './signUp.component';
+import { MainPage } from '../home/main';
 
 @Component({
   templateUrl: 'build/pages/user/homePage.html'
@@ -15,5 +16,8 @@ export class HomePage {
   }
   navSignUp(){
   	this.nav.push(SignUpPage)
+  }
+  navMain(){
+  	this.nav.setRoot(MainPage);
   }
 }
